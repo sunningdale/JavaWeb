6c245ef3bfd65a0ef7e69f17a82add856bcce9a4
sunningdale <cxy3391439@163.com>
2014-09-25 16:59:07 +0800

    modify

--- springtest/src/main/java/com/test/LoginController.java
+++ springtest/src/main/java/com/test/LoginController.java
@@ -5,20 +5,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
-@RequestMapping("/index.html")
+//@RequestMapping("/login/index.html")
 public class LoginController {
 	
-	@RequestMapping(method=RequestMethod.GET)
+	@RequestMapping(value="index.do")
 	public ModelAndView doGet(HttpServletRequest request, HttpServletResponse reponse){
 		
 		System.out.println("-------------------login---------------");
 		
-		return new ModelAndView("index");
+		return new ModelAndView("/login/index");
 	}
 
 }
